cb6d82cfb92f0a603014eada647950f5513a21e0
Eber <www.eberacunaquispe@gmail.com>
2026-07-13 22:08:06 -0500

    Se añadio el menu de la cola lo que hace es listar las musicas en reproduccion y ver cual sigue en el turno

--- src/controlador/ControladorMusica.java
+++ src/controlador/ControladorMusica.java
@@ -47,9 +47,9 @@ public class ControladorMusica {
                     break;
 
                 case 4:
-                    System.out.println("\nCola (En construcción)");
-                    pausa();
+                    menuCola();
                     break;
+
 
                 case 5:
                     System.out.println("\nCola Prioridad (En construcción)");
@@ -292,6 +292,103 @@ public class ControladorMusica {
 
                     System.out.println();
                     gestor.mostrarHistorial();
+
+                    pausa();
+
+                    break;
+
+                case 0:
+                    break;
+
+                default:
+
+                    System.out.println("\nOpción incorrecta.");
+                    pausa();
+
+            }
+
+        } while (op != 0);
+
+    }
+
+    private void menuCola() {
+
+        int op;
+
+        do {
+
+            System.out.println("\n==============================");
+            System.out.println("    COLA DE REPRODUCCIÓN");
+            System.out.println("==============================");
+            System.out.println("1. Agregar canción a la cola");
+            System.out.println("2. Reproducir siguiente canción");
+            System.out.println("3. Ver siguiente canción");
+            System.out.println("4. Mostrar cola");
+            System.out.println("0. Regresar");
+            System.out.print("Opción: ");
+
+            op = leerEntero();
+
+            switch (op) {
+
+                case 1:
+
+                    System.out.print("Ingrese el ID de la canción: ");
+                    int id = leerEntero();
+
+                    Cancion c = gestor.getCanciones().buscar(id);
+
+                    if (c != null) {
+
+                        gestor.agregarCola(c);
+
+                    } else {
+
+                        System.out.println("\nNo existe una canción con ese ID.");
+
+                    }
+
+                    pausa();
+
+                    break;
+
+                case 2:
+
+                    Cancion reproducida = gestor.atenderCola();
+
+                    if (reproducida != null) {
+
+                        System.out.println("\nReproduciendo:");
+                        System.out.println(reproducida);
+
+
+                        gestor.agregarHistorial(reproducida);
+
+                    }
+
+                    pausa();
+
+                    break;
+
+                case 3:
+
+                    Cancion siguiente = gestor.getCola().peek();
+
+                    if (siguiente != null) {
+
+                        System.out.println("\nSiguiente canción:");
+                        System.out.println(siguiente);
+
+                    }
+
+                    pausa();
+
+                    break;
+
+                case 4:
+
+                    System.out.println();
+                    gestor.mostrarCola();
 
                     pausa();
 
