153126641b0f622aaa61dc6f1a4616e239afd513
Eber <www.eberacunaquispe@gmail.com>
2026-07-13 19:36:47 -0500

    Se implemento la interfaz comparable lo que hace esto es que ordena las canciones por medio del titulo

--- src/modelo/Cancion.java
+++ src/modelo/Cancion.java
@@ -1,84 +1,100 @@
 
 package modelo;
 
+public class Cancion implements Comparable<Cancion> {
 
-public class Cancion {
     private static int contador = 1;
-    
+
     private final int id;
     private String titulo;
     private String artista;
     private String album;
     private int duracion;
     private String genero;
-    
-    
-    public Cancion(String titulo){
-        this.titulo = titulo;
+
+    public Cancion(String titulo) {
         this.id = contador++;
-    };
-    
-    public Cancion( String titulo, String artista,String album, int duracion, String genero){
+        this.titulo = titulo;
+        this.artista = "Desconocido";
+        this.album = "Sin álbum";
+        this.duracion = 0;
+        this.genero = "Sin género";
+    }
+
+    public Cancion(String titulo,
+                   String artista,
+                   String album,
+                   int duracion,
+                   String genero) {
+
         this.id = contador++;
         this.titulo = titulo;
         this.artista = artista;
         this.album = album;
         this.duracion = duracion;
         this.genero = genero;
+
     }
-    
-    
-    
+
     @Override
     public String toString() {
-        return "[" + id + "] " + titulo + " - " + artista + " - " + genero + " (" + album + ", " + duracion + "s)";
+
+        return "[" + id + "] "
+                + titulo
+                + " - "
+                + artista;
+
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
+    @Override
+    public int compareTo(Cancion otra) {
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+        return this.titulo.compareToIgnoreCase(otra.titulo);
 
-    public String getArtista() {
-        return artista;
-    }
-
-    public void setArtista(String artista) {
-        this.artista = artista;
-    }
-
-    public String getAlbum() {
-        return album;
-    }
-
-    public void setAlbum(String album) {
-        this.album = album;
-    }
-
-    public int getDuracion() {
-        return duracion;
-    }
-
-    public void setDuracion(int duracion) {
-        this.duracion = duracion;
     }
 
     public int getId() {
         return id;
     }
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
 
     public String getGenero() {
         return genero;
     }
 
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
     public void setGenero(String genero) {
         this.genero = genero;
     }
-    
-   
-    
+
 }
