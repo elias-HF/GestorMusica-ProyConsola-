77d822b0280b6d34426fd4230aef4880e39cf4f6
Eber <www.eberacunaquispe@gmail.com>
2026-07-13 19:40:35 -0500

    Se implemento un menu mas ordenado que accede a todas las clases de manera general e intuitiva

--- src/vista/Menu.java
+++ src/vista/Menu.java
@@ -4,22 +4,27 @@
  */
 package vista;
 
-/**
- *
- * @author USUARIO
- */
+
 public class Menu {
-    String menu = "===================Sistema de gestión de Musica =====================\n" +
-"Registrar canción en la biblioteca principal (Lista enlazada / AVL)\n" +
-"Buscar canción por ID (ABB / AVL)\n" +
-"Agregar canción a la cola de reproducción (Cola)\n" +
-"Agregar canción a favoritos (Cola con prioridad)\n" +
-"Reproducir siguiente canción (Desencolar y apilar en historial)\n" +
-"Reproducir canción anterior (Desapilar de historial)\n" +
-"Ver lista de reproducción actual y bucle (Lista doble/circular)\n" +
-"Ver historial de canciones escuchadas (Pila)\n" +
-"Explorar canciones por géneros musicales (Árbol general)\n" +
-"Ver biblioteca completa ordenada (Recorridos de Árbol)\n" +
-"Salir del sistema\n" +
-"Seleccione una opción: ";
+
+    public void menuPrincipal() {
+
+        System.out.println();
+        System.out.println("==================================================");
+        System.out.println("            SISTEMA GESTOR DE MUSICA");
+        System.out.println("==================================================");
+        System.out.println("1. Lista Simple");
+        System.out.println("2. Lista Doble");
+        System.out.println("3. Pila");
+        System.out.println("4. Cola");
+        System.out.println("5. Cola Prioridad");
+        System.out.println("6. Árbol Binario");
+        System.out.println("7. ABB");
+        System.out.println("8. AVL");
+        System.out.println("9. Árbol General");
+        System.out.println("0. Salir");
+        System.out.println("==================================================");
+
+    }
+
 }
