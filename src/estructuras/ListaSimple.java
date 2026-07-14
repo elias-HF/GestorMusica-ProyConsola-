ec95fec70f1a46aa31c80c20a7c5a4f5be6aac61
Eber <www.eberacunaquispe@gmail.com>
2026-07-13 21:35:46 -0500

    Se añadio los metodos de buscar y eliminar por id

--- src/estructuras/ListaSimple.java
+++ src/estructuras/ListaSimple.java
@@ -10,42 +10,115 @@ import modelo.Cancion;
  *
  * @author EBER
  */
+
 public class ListaSimple {
 
     class Nodo {
+
         Cancion cancion;
         Nodo siguiente;
 
         Nodo(Cancion cancion) {
             this.cancion = cancion;
-            this.siguiente = null;
         }
+
     }
 
     private Nodo cabeza;
 
     public void agregar(Cancion cancion) {
+
         Nodo nuevo = new Nodo(cancion);
 
         if (cabeza == null) {
             cabeza = nuevo;
-        } else {
-            Nodo aux = cabeza;
-
-            while (aux.siguiente != null) {
-                aux = aux.siguiente;
-            }
-
-            aux.siguiente = nuevo;
+            return;
         }
+
+        Nodo aux = cabeza;
+
+        while (aux.siguiente != null)
+            aux = aux.siguiente;
+
+        aux.siguiente = nuevo;
+
     }
 
-    public void mostrar() {
+    public Cancion buscar(int id) {
+
         Nodo aux = cabeza;
 
         while (aux != null) {
-            System.out.println("- " + aux.cancion);
+
+            if (aux.cancion.getId() == id)
+                return aux.cancion;
+
             aux = aux.siguiente;
+
         }
+
+        return null;
+
     }
+
+    public boolean eliminar(int id) {
+
+        if (cabeza == null)
+            return false;
+
+        if (cabeza.cancion.getId() == id) {
+
+            cabeza = cabeza.siguiente;
+            return true;
+
+        }
+
+        Nodo anterior = cabeza;
+        Nodo actual = cabeza.siguiente;
+
+        while (actual != null) {
+
+            if (actual.cancion.getId() == id) {
+
+                anterior.siguiente = actual.siguiente;
+                return true;
+
+            }
+
+            anterior = actual;
+            actual = actual.siguiente;
+
+        }
+
+        return false;
+
+    }
+
+    public boolean estaVacia() {
+
+        return cabeza == null;
+
+    }
+
+    public void mostrar() {
+
+        if (estaVacia()) {
+
+            System.out.println("No existen canciones.");
+            return;
+
+        }
+
+        Nodo aux = cabeza;
+
+        while (aux != null) {
+
+            System.out.println(aux.cancion);
+
+            aux = aux.siguiente;
+
+        }
+
+    }
+
 }
